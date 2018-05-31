5c98b8ae320d2c55d997ef710438a95dcb978603
jhassler1031 <jhassler1031@gmail.com>
2018-05-31 15:35:27 -0400

    changed comments

--- sports_api.py
+++ sports_api.py
@@ -15,11 +15,13 @@ def show_json():
     return jsonify(list)
 
 #Display data from sports_stats table in sports_db
+#=========================================================================
 
 @app.route("/")
 def show_teams():
     return jsonify(db.query("SELECT * FROM sports_stats;").export('json'))
 
+#==========================================================================
 #long way below
 """
 @app.route("/")
