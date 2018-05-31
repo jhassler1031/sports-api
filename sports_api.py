00ad3118840c34af45ee4484a93e29e80e438b40
jhassler1031 <jhassler1031@gmail.com>
2018-05-31 13:23:08 -0400

    created a list, printed jsonified version to browser

--- sports_api.py
+++ sports_api.py
@@ -5,6 +5,13 @@ db = records.Database("postgres://localhost/sports_db")
 
 teams = db.query("SELECT * FROM sports_stats;")
 
-@app.route("/")
-def show_teams():
+#first part of homework
+@app.route("/first")
+def show_html():
     return "<div>Hello world!</div>"
+
+#updated to output JSON instead of HTML
+@app.route("/second")
+def show_json():
+    list = [1, 2, 3, "this is a string"]
+    return jsonify(list)
