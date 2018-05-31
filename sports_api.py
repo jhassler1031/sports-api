2ba818f364f61469c7d433c1a6104d36d3362e3c
jhassler1031 <jhassler1031@gmail.com>
2018-05-31 14:03:55 -0400

    converting all record items to str and is working but need to keep original data, changing from list to dict

--- sports_api.py
+++ sports_api.py
@@ -3,8 +3,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 db = records.Database("postgres://localhost/sports_db")
 
-teams = db.query("SELECT * FROM sports_stats;")
-
 #first part of homework
 @app.route("/first")
 def show_html():
@@ -15,3 +13,21 @@ def show_html():
 def show_json():
     list = [1, 2, 3, "this is a string"]
     return jsonify(list)
+
+#Display data from sports_stats table in sports_db
+@app.route("/")
+def show_teams():
+    teams = db.query("SELECT * FROM sports_stats;")
+    team_list = []
+    for team in teams:
+        row_list = []
+        for item in team:
+            row_list.append(str(item))
+        team_list.append(row_list)
+    return jsonify(team_list)
+
+"""
+teams = db.query("SELECT * FROM sports_stats;")
+team_list = teams.as_dict()
+print*(team_list)
+"""
